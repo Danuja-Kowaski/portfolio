c0b29eb2dd9860eff7e1dea9cbdb90c1dc8edbc8
Danuja Silva <danuja.2018221@iit.ac.lk>
2022-08-02 02:58:50 +0530

    added changes with gh-pages

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -52,8 +52,7 @@ const SocialLinks =() =>{
     <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
         {links.map(({ id, child, href, style, download }) => (
-          <li
-            key={id}
+          <li key={id}
             className={
               "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300" +
               " " +
